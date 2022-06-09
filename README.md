4bb9f4b42b60fc1d7f32668926a19a105f961224
Miriã Cristina <107210330+miicristina@users.noreply.github.com>
2022-06-09 23:53:58 +0000

    Finalização das atividades.

---
+++ README.md
@@ -0,0 +1,6 @@
+# ESTRUTURAS NO PYTHON
+_Trabalhamos Estrutura de Decisão e Estrutura de Repetição_
+_com acompanhamento de slides e exercícios práticos._  
+_-> Projeto feito no Replit e linkado no GitHub._  
+***Aluna Miriã Cristina***
+:sparkles:
---
+++ main.py
@@ -0,0 +1,12 @@
+__author__ = "Miriã Cristina"
+import Estrutura_de_decisão
+print(Estrutura_de_decisão)
+
+import Ex_Estru_decisão
+print(Ex_Estru_decisão)
+
+import Estrutura_de_repetição
+print(Estrutura_de_repetição)
+
+import Ex_Estru_repetição
+print(Ex_Estru_repetição)
